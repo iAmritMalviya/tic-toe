ba2866613584821926ade2960bda76bb800bf273
iAmritMalviya <malviyaamrat42@gmail.com>
2023-05-26 23:26:15 +0530

    React init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+function Square({value, nextSquareClick}) {
+  return <button className="square" onClick={handleClick}>{value}</button>
+}
 
 function App() {
+  const [square, setSquare] = useState(Array(9).fill(null))
+
+  function handleClick() {
+    const nextSquares = square.slice()
+    nextSquares[0] = 'X'
+    setSquare(nextSquares) 
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="board-row">
+        <Square value={square[0]}/>
+        <Square value={square[1]}/>
+        <Square value={square[2]}/>
+
+      </div>
+      <div className="board-row">
+        <Square value={square[3]}/>
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </div>
   );
 }
